287d37a1074eb7e6c594b48371244e116179abb2
David Jaress <david.jaress@gmail.com>
2015-05-10 00:36:27 -0500

    Create data directory

--- wiki.go
+++ wiki.go
@@ -20,12 +20,12 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-  filename := "templates/" + p.Title + ".txt"
+  filename := "data/" + p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-  filename := "templates/" + title + ".txt"
+  filename := "data/" + title + ".txt"
   body, err := ioutil.ReadFile(filename)
   if err != nil {
     return nil, err
